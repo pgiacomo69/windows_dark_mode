721cdef82e65753c6abb6f816eac329ccf22f353
Giacomo Policicchio <pgiacomo69@yahoo.com>
2022-03-31 01:15:17 +0200

    Windows Example

---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <windows_dark_mode/windows_dark_mode_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  WindowsDarkModePluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("WindowsDarkModePlugin"));
+}
